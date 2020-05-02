b2460d3194993ddc88a6c557198de7e5cb0b06f8
Karlafcs <49644546+Karlafcs@users.noreply.github.com>
2020-05-02 17:25:28 -0300

    Add files via upload

--- rfiles/q2_tree_baseline.R
+++ rfiles/q2_tree_baseline.R
@@ -1,9 +1,9 @@
 
-tree_base <- rpart(formula=estado~case_in_country + reporting.date + country +
-                     gender + age + symptom_onset + If_onset_approximated +
+tree_base <- rpart(formula=estado~case_in_country + reporting.date + 
+                     age + symptom_onset + If_onset_approximated +
                      hosp_visit_date + international_traveler + domestic_traveler +
                      exposure_start + exposure_end + traveler + visiting.Wuhan +
-                     from.Wuhan, 
+                     from.Wuhan + male + female + idh, 
                    data=covid_treino, method="class",
                    control=rpart.control(minsplit=2, cp=0.0, xval = 10),
                    parms= list(split="information"))
